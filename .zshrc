7fd72dee0a99f29d40a092005902499b7edcc3a8
protoget <weikqin@gmail.com>
2019-01-21 12:50:24 -0800

    Rename .zshrc.me to .zshrc.me.sh for better syntax highlighting

--- .zshrc
+++ .zshrc
@@ -93,4 +93,4 @@ bindkey -v
 # Add RVM to PATH for scripting. Make sure this is the last PATH variable change.
 export PATH="$PATH:$HOME/.rvm/bin"
 
-source ${HOME}/.dotfiles/.zshrc.me
+source ${HOME}/.dotfiles/.zshrc.me.sh
--- .zshrc.me.sh
+++ .zshrc.me.sh
@@ -16,3 +16,13 @@ alias byobu_resize='/usr/lib/byobu/include/tmux-detach-all-but-current-client'
 
 # Show ubuntu version.
 alias ubuntu_version='lsb_release -a'
+
+# Vim
+vim_plugin() {
+  local cmd=$1
+  if [[ $cmd != install ]]; then
+    echo 'Only "install" cmd is supported for now'
+    exit 1
+  fi
+  vim +PluginInstall +qall
+}
