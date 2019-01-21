1a3528fc06c3c4ff9213277cd512122191be254d
protoget <weikqin@gmail.com>
2019-01-21 00:04:39 -0800

    Tentative script to insall power font for <Ubuntu 14.10

--- install.sh
+++ install.sh
@@ -33,7 +33,13 @@ if [[ $(get_os) == "Mac" ]]; then
   cd ..
   rm -rf fonts
 else
-  sudo apt-get install fonts-powerline
+  if ! sudo apt-get install fonts-powerline
+  then
+    wget https://github.com/Lokaltog/powerline/raw/develop/font/PowerlineSymbols.otf https://github.com/Lokaltog/powerline/raw/develop/font/10-powerline-symbols.conf
+    mkdir -p ~/.fonts/ && mv PowerlineSymbols.otf ~/.fonts/
+    fc-cache -vf ~/.fonts
+    mkdir -p ~/.config/fontconfig/conf.d/ && mv 10-powerline-symbols.conf ~/.config/fontconfig/conf.d/
+  fi
 fi
 
 
